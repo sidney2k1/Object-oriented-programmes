fd3de526861ff6423e34c90adcbdd113897e943d
sidney2k1 <sidsingla02@gmail.com>
2024-11-05 17:14:18 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self,maxspeed,mileage):
+        self.maxspeed=maxspeed
+        self.mileage=mileage
+modelx=Vehicle(240,18)
+print("Maximum speed of modelx:",modelx.maxspeed)
+print("Mileage of modelx:",modelx.mileage)
